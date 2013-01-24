45f53ff7450ff6db3eb8d798482805f22ffc8764
adam <financeCoding@gmail.com>
2013-01-23 23:34:09 -0800

    added comments.

--- lib/google_oauth2_console.dart
+++ lib/google_oauth2_console.dart
@@ -1,5 +1,6 @@
 library google_oauth2_console;
-// TODO: re enable secure cert checking with curl.
+// TODO(adam): allow client to be run with out cert file but warn the user
+// about launching insecure mode.
 import "src/console/oauth2_console_client/oauth2.dart";
 export "src/console/oauth2_console_client/oauth2.dart";
 import "src/console/oauth2_console_client/system_cache.dart";
