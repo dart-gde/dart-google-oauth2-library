29e39484ca9570386ad7f4b34c15062863913183
adam <financeCoding@gmail.com>
2013-01-24 06:41:34 -0800

    removed todo

--- lib/google_oauth2_console.dart
+++ lib/google_oauth2_console.dart
@@ -1,6 +1,5 @@
 library google_oauth2_console;
-// TODO(adam): allow client to be run with out cert file but warn the user
-// about launching insecure mode.
+
 import "src/console/oauth2_console_client/oauth2.dart";
 export "src/console/oauth2_console_client/oauth2.dart";
 import "src/console/oauth2_console_client/system_cache.dart";
