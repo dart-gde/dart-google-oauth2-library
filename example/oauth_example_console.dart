1df4049d6175fb2017610034c5bad1a135f60488
Kevin Moore <kevin@thinkpixellab.com>
2013-10-01 10:58:21 -0300

    massive cleanup to oauth console example

--- example/oauth_example_console.dart
+++ example/oauth_example_console.dart
@@ -3,36 +3,33 @@ import "dart:json" as JSON;
 import "package:google_oauth2_client/google_oauth2_console.dart";
 import "package:http/http.dart" as http;
 
+const _IDENTIFIER =
+  "299615367852-n0kfup30mfj5emlclfgud9g76itapvk9.apps.googleusercontent.com";
+const _SECRET = "azeFTOjszzL57dvMd-JS2Zda";
+const _SCOPES = const ["https://www.googleapis.com/auth/plus.login"];
+const _REQUEST_VISIBLE_ACTIONS = const[
+        "http://schemas.google.com/AddActivity",
+        "http://schemas.google.com/CreateActivity"];
+
 void main() {
-  String identifier = "299615367852-n0kfup30mfj5emlclfgud9g76itapvk9.apps.googleusercontent.com";
-  String secret = "azeFTOjszzL57dvMd-JS2Zda";
-  List scopes = ["https://www.googleapis.com/auth/plus.login"];
-  List request_visible_actions = ["http://schemas.google.com/AddActivity", "http://schemas.google.com/CreateActivity"];
-  var auth = new OAuth2Console(identifier: identifier, secret: secret, scopes: scopes, request_visible_actions: request_visible_actions);
+  var auth = new OAuth2Console(identifier: _IDENTIFIER,
+      secret: _SECRET,
+      scopes: _SCOPES,
+      request_visible_actions: _REQUEST_VISIBLE_ACTIONS);
 
-  Future clientCallback(http.Client client) {
-    var completer = new Completer();
-    final url = "https://www.googleapis.com/plus/v1/people/me";
-    client.get(url).then((http.Response response) {
-      var data = JSON.parse(response.body);
-      var c = "Logged in as ${data["displayName"]}";
-      print(c);
-      completer.complete(c);
+  auth.withClient(_clientCallback)
+    .then((_) {
+      print('done');
+      auth.close();
     });
-    return completer.future;
-  };
+}
 
-  auth.withClient(clientCallback).whenComplete(() {
-    print("done");
-    auth.close();
-//    auth = null;
-// Try for a second time.
-//    auth = new OAuth2Console(identifier: identifier, secret: secret, scopes: scopes);
-//    auth.withClient(clientCallback).whenComplete(() {
-//      print("done2");
-//      auth.close();
-//      auth = null;
-//    });
+Future _clientCallback(http.Client client) {
+  final url = "https://www.googleapis.com/plus/v1/people/me";
+  return client.get(url).then((http.Response response) {
+    var data = JSON.parse(response.body);
+    var c = "Logged in as ${data["displayName"]}";
+    print(c);
+    return c;
   });
-
 }
