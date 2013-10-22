60efed019164bc81c2dc0d8d2af0f04c3360aca7
adam <financeCoding@gmail.com>
2013-10-21 20:04:02 -0700

    removing logging from being exported.

--- lib/google_oauth2_console.dart
+++ lib/google_oauth2_console.dart
@@ -1,5 +1,4 @@
 library google_oauth2_console;
 
 export "src/console/oauth2_console_client/oauth2.dart";
-export "src/console/oauth2_console_client/log.dart";
 export "src/common/url_pattern.dart";
